59da68a45ea050dd9935b8bdfc40853a0b3810b2
Cyberwolf098 <95936532+Cyberwolf098@users.noreply.github.com>
2023-11-14 09:33:58 -0500

    affichage

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def main():
     encrypted_text = encrypt(text_to_encrypt, key)
     print(f"Encrypted text: {encrypted_text}")
 
-    # decrypted_text = decrypt(encrypted_text, key)
-    # print(f"Decrypted text: {decrypted_text}")
+    decrypted_text = decrypt(encrypted_text, key)
+    print(f"Decrypted text: {decrypted_text}")
 
 if __name__ == "__main__":
     main()
