630733db32476864ebe95815096896642da7feb7
Cyberwolf098 <95936532+Cyberwolf098@users.noreply.github.com>
2023-11-14 09:27:54 -0500

    fnc decrypt ajou

--- encryption.py
+++ encryption.py
@@ -12,7 +12,7 @@ def encrypt(text, key):
     encrypted_text = fernet.encrypt(text.encode())
     return urlsafe_b64encode(encrypted_text)
 
-# def decrypt(encrypted_text, key):
-#     fernet = Fernet(key)
-#     decrypted_text = fernet.decrypt(urlsafe_b64decode(encrypted_text)).decode('utf-8')
-#     return decrypted_text
+ def decrypt(encrypted_text, key):
+     fernet = Fernet(key)
+     decrypted_text = fernet.decrypt(urlsafe_b64decode(encrypted_text)).decode('utf-8')
+     return decrypted_text
